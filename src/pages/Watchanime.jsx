0741217c188ed7ed016a6ace0726171fe83805d9
CommitTODev <abhishekshivale45@gmail.com>
2024-02-19 19:19:02 +0530

    added button to watch anime in dub

--- src/pages/Watchanime.jsx
+++ src/pages/Watchanime.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import "./animeInfo.css";
 import VideoPlayer from "../components/videoplayer";
 
 function Watchanime() {
-  const [epNo, setEpNo] = useState(1);
+  const navigate = useNavigate();
   const [epArry, setepArry] = useState([]);
   const [streamUrl, setStreamUrl] = useState("");
   const { animeid, episodes } = useParams();
   const decodedAnimeId = decodeURIComponent(animeid);
   const decodedEpisodeId = decodeURIComponent(episodes);
-  const cleanAnimeId = decodedAnimeId.replace("animeid=", "");
+  const id = decodedAnimeId.replace("animeid=", "");
+  const [cleanAnimeId, setcleanAnimeId] = useState(id);
   const cleanEpisodeId = decodedEpisodeId.replace("episodes=", "");
 
   function ButtonCompo(props) {
@@ -28,34 +29,40 @@ function Watchanime() {
     );
   }
 
-  useEffect(
-    () => {
-      async function fetchAnimeData() {
-        try {
-          const episodeResponse = await axios.get(
-            `https://api.abhishekshivale45.workers.dev/episode/${cleanAnimeId}-episode-${cleanEpisodeId}`
-          );
-          const animeResponse = await axios.get(
-            `https://api.abhishekshivale45.workers.dev/anime/${cleanAnimeId}`
-          );
-          const epdata = episodeResponse.data.results.stream.sources[0].file;
-          setStreamUrl(epdata);
-          let len = animeResponse.data.results.episodes;
-          setepArry(len);
-        } catch (error) {
-          console.error("Error fetching data:", error.message);
-        }
-      }
+  const handleDubButtonClick = () => {
+    const searchParams = new URLSearchParams(window.location.search);
+    searchParams.set("animeid", `${cleanAnimeId}-dub`);
+    searchParams.set("episodes", cleanEpisodeId);
+    const searchString = searchParams.toString();
+    const modifiedSearchString = searchString.replace("&", "/");
+    window.location.href = `/watch/${modifiedSearchString}`;
+  };
 
-      fetchAnimeData();
-    },
-    [cleanAnimeId, cleanEpisodeId],
-    ButtonCompo
-  );
+  useEffect(() => {
+    async function fetchAnimeData() {
+      try {
+        const episodeResponse = await axios.get(
+          `https://api.abhishekshivale45.workers.dev/episode/${cleanAnimeId}-episode-${cleanEpisodeId}`
+        );
+        const animeResponse = await axios.get(
+          `https://api.abhishekshivale45.workers.dev/anime/${cleanAnimeId}`
+        );
+        const epdata = episodeResponse.data.results.stream.sources[0].file;
+        setStreamUrl(epdata);
+        let len = animeResponse.data.results.episodes;
+        setepArry(len);
+      } catch (error) {
+        console.error("Error fetching data:", error.message);
+      }
+    }
+
+    fetchAnimeData();
+  }, [cleanAnimeId, cleanEpisodeId]);
+
   return (
     <div className="container m-auto sm:flex max-w-[1300px]">
       <div className="w-full bg-none">
-      <VideoPlayer url={streamUrl} />
+        <VideoPlayer url={streamUrl} />
       </div>
       <div className="bg-black sm:basis-4/12 overflow-scroll aspect-video scrollbar-hidden ">
         <div className="flex flex-wrap bg-black mx-2 my-3">
@@ -63,6 +70,17 @@ function Watchanime() {
             <ButtonCompo bt={pop} link={cleanAnimeId} />
           ))}
         </div>
+        {cleanAnimeId.includes("dub") ? (
+          <></>
+        ) : (
+          <>
+            <div
+              className="px-24 bg-red-600 text-center text-white"
+              onClick={handleDubButtonClick}>
+              Watch In dub
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
