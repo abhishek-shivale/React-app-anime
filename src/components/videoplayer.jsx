c5182729a56653e348117fbe8812f2c997fed96a
CommitTODev <abhishekshivale45@gmail.com>
2024-02-19 18:31:13 +0530

    added custom player

--- src/components/videoplayer.jsx
+++ src/components/videoplayer.jsx
@@ -1,37 +1,62 @@
 import React, { useEffect, useRef, useState } from "react";
-import Hls from 'hls.js';
-
+import Hls from "hls.js";
+import Plyr from "plyr";
+import "plyr-react/plyr.css";
+import "./videoplayer.css";
 function VideoPlayer(props) {
-  const [url, setUrl] = useState('');
+  const [url, setUrl] = useState(props.url);
+  const videoRef = useRef(null);
+  let player = null;
 
   useEffect(() => {
     setUrl(props.url);
   }, [props.url]);
 
-  const videoRef = useRef(null);
-
-
   useEffect(() => {
     const video = videoRef.current;
 
-    if (video) {
-      if (Hls.isSupported()) {
-        const hls = new Hls();
-        hls.loadSource(url);
-        hls.attachMedia(video);
-      } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-        video.src = url;
-      }
+    if (!video || !url) return;
+
+    // Initialize HLS.js if supported
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(url);
+      hls.attachMedia(video);
+
+      // Listen for manifest parsed event to get available qualities
+      hls.on(Hls.Events.MANIFEST_PARSED, (event, data) => {
+        const availableQualities = hls.levels.map((l) => l.height);
+
+        // Construct Plyr quality options
+        const qualityOptions = {
+          quality: {
+            default: availableQualities[0],
+            options: availableQualities,
+            forced: true,
+            onChange: (quality) => handleQualityChange(quality),
+          },
+        };
+
+        // Initialize Plyr with quality options
+        player = new Plyr(video, { ...qualityOptions });
+      });
+    } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+      video.src = url;
+      player = new Plyr(video);
     }
+
+    return () => {
+      if (player) {
+        player.destroy();
+      }
+    };
   }, [url]);
 
-  return (
-    <>
-    <div className="relative hover: ">
-      <video ref={videoRef} className="object-cover video aspect-video w-full" controls />
-    </div>
-    </>
-  );
+  const handleQualityChange = (quality) => {
+    console.log("Selected quality:", quality);
+  };
+
+  return <video ref={videoRef} className="plyr video" />;
 }
 
 export default VideoPlayer;
--- src/pages/Watchanime.jsx
+++ src/pages/Watchanime.jsx
@@ -1,65 +1,71 @@
-import React, { useEffect, useState,useRef } from 'react';
-import axios from 'axios';
-import { Link, useParams } from 'react-router-dom';
-import './animeInfo.css'
-import VideoPlayer from '../components/videoplayer';
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+import "./animeInfo.css";
+import VideoPlayer from "../components/videoplayer";
 
 function Watchanime() {
-    const [epNo, setEpNo] = useState(1);
-    const [epArry, setepArry] = useState([])
-    const [streamUrl, setStreamUrl] = useState('');
-    const { animeid, episodes } = useParams();
-    const decodedAnimeId = decodeURIComponent(animeid);
-    const decodedEpisodeId = decodeURIComponent(episodes);
-    const cleanAnimeId = decodedAnimeId.replace('animeid=', '');
-    const cleanEpisodeId = decodedEpisodeId.replace('episodes=', '');
+  const [epNo, setEpNo] = useState(1);
+  const [epArry, setepArry] = useState([]);
+  const [streamUrl, setStreamUrl] = useState("");
+  const { animeid, episodes } = useParams();
+  const decodedAnimeId = decodeURIComponent(animeid);
+  const decodedEpisodeId = decodeURIComponent(episodes);
+  const cleanAnimeId = decodedAnimeId.replace("animeid=", "");
+  const cleanEpisodeId = decodedEpisodeId.replace("episodes=", "");
 
-function ButtonCompo(props){
-    const text = props.bt
-    const arry = text[0]
+  function ButtonCompo(props) {
+    const text = props.bt;
+    const arry = text[0];
     return (
-        <>
+      <>
         <Link to={`/watch/animeid=${props.link}/episodes=${arry}`}>
-        <div className='flex'>
-        <button className='text-white h-10 w-14 border'>{arry}</button>
-        </div>
+          <div className="flex">
+            <button className="text-white h-10 w-14 border">{arry}</button>
+          </div>
         </Link>
-        </>
-    )
-}
-
-useEffect(() => {
-    async function fetchAnimeData() {
-        try {
-            const episodeResponse = await axios.get(`https://api.abhishekshivale45.workers.dev/episode/${cleanAnimeId}-episode-${cleanEpisodeId}`);
-            const animeResponse = await axios.get(`https://api.abhishekshivale45.workers.dev/anime/${cleanAnimeId}`);
-            const epdata = episodeResponse.data.results.stream.sources[0].file;
-            setStreamUrl(epdata);
-            let len = animeResponse.data.results.episodes
-            setepArry(len)
-        } catch (error) {
-            console.error('Error fetching data:', error.message);
-        }
-    }
-
-    fetchAnimeData();
-}, [cleanAnimeId, cleanEpisodeId], ButtonCompo);
-    return (
-  <div className='container m-auto sm:flex max-w-[1300px]'>
-    {/* <div className='basis-4/12 aspect-video'></div> */}
-    <div className='w-full' >
-     <VideoPlayer url={streamUrl} />
-     </div>
-     <div className='bg-black sm:basis-4/12 overflow-scroll aspect-video scrollbar-hidden '>
-        <div className='flex flex-wrap bg-black mx-2 my-3'>
-        {epArry.map((pop)=>(
-            <ButtonCompo  bt={pop} link={cleanAnimeId} />
-        ))}
-        </div>
-     </div>
-     
-  </div>
+      </>
     );
+  }
+
+  useEffect(
+    () => {
+      async function fetchAnimeData() {
+        try {
+          const episodeResponse = await axios.get(
+            `https://api.abhishekshivale45.workers.dev/episode/${cleanAnimeId}-episode-${cleanEpisodeId}`
+          );
+          const animeResponse = await axios.get(
+            `https://api.abhishekshivale45.workers.dev/anime/${cleanAnimeId}`
+          );
+          const epdata = episodeResponse.data.results.stream.sources[0].file;
+          setStreamUrl(epdata);
+          let len = animeResponse.data.results.episodes;
+          setepArry(len);
+        } catch (error) {
+          console.error("Error fetching data:", error.message);
+        }
+      }
+
+      fetchAnimeData();
+    },
+    [cleanAnimeId, cleanEpisodeId],
+    ButtonCompo
+  );
+  return (
+    <div className="container m-auto sm:flex max-w-[1300px]">
+      <div className="w-full bg-none">
+      <VideoPlayer url={streamUrl} />
+      </div>
+      <div className="bg-black sm:basis-4/12 overflow-scroll aspect-video scrollbar-hidden ">
+        <div className="flex flex-wrap bg-black mx-2 my-3">
+          {epArry.map((pop) => (
+            <ButtonCompo bt={pop} link={cleanAnimeId} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Watchanime;
